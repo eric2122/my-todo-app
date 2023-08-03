c70a127a018f58efc68818ea5f752a7cbf9eac05
eric <127749763+eric2122@users.noreply.github.com>
2023-08-03 10:47:11 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -27,29 +27,7 @@ function TodoApp() {
     'https://placekitten.com/1938/1098',
     'https://placekitten.com/1939/1099',
     'https://placekitten.com/1940/1100',
-    'https://placekitten.com/1941/1101',
-    'https://placekitten.com/1942/1102',
-    'https://placekitten.com/1943/1103',
-    'https://placekitten.com/1943/1103',
-    'https://placekitten.com/1944/1104',
-    'https://placekitten.com/1945/1105',
-    'https://placekitten.com/1946/1106',
-    'https://placekitten.com/1947/1107',
-    'https://placekitten.com/1948/1108',
-    'https://placekitten.com/1949/1109',
-    'https://placekitten.com/1950/1110',
-    'https://placekitten.com/1951/1111',
-    'https://placekitten.com/1952/1112',
-    'https://placekitten.com/1953/1113',
-    'https://placekitten.com/1954/1114',
-    'https://placekitten.com/1955/1115',
-    'https://placekitten.com/1956/1116',
-    'https://placekitten.com/1957/1117',
-    'https://placekitten.com/1958/1118',
-    'https://placekitten.com/1959/1119',
-    'https://placekitten.com/1960/1120',
-    'https://placekitten.com/1961/1121',
-    'https://placekitten.com/1962/1122'
+ 
     
   ];
 
